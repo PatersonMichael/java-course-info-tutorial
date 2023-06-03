31d4ed9d356e79bc8a461dfc4b4e9de0e21554e4
PatersonMichael <michael.paterson7@gmail.com>
2023-06-03 15:37:39 -0700

    refactored POM files to add dependency management, shade plugin for maven

--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -5,18 +5,48 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
+import java.util.logging.LogManager;
 
 public class CourseServer {
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
-    private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String... args) {
-        LOG.info("Starting HTTP Server");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFilename = loadDataBaseFilename();
+        LOG.info("Starting HTTP Server with database {}", databaseFilename);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFilename);
         ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
 
-        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(loadBaseURI()), config);
+    }
+
+    private static String loadDataBaseFilename() {
+        try (InputStream propertiesStream =
+                CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("course-info.database");
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load database filename", e);
+        }
+    }
+    private static String loadBaseURI() {
+        try (InputStream propertiesStream =
+                CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("base-URI");
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load base URI", e);
+        }
     }
 }
